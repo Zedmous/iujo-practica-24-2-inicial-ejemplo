03b24b28b02460b572f83a36c9f4d00b9fc7062f
Zedmous <zedmous@gmail.com>
2024-11-07 19:56:58 -0400

    configuracion del ignore

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+
+const colors = require('colors');
 const { Matematica } = require("./clases/estadistica");
+
+
 const estadistica = new Matematica();
-console.log("El factorial del numero es: ",estadistica.factorial(4));
-console.log("La combinatoria es: ",estadistica.combinatoria(0,0))
+console.log("El factorial del numero es: ".green,estadistica.factorial(4));
+console.log("La combinatoria es: ".blue,estadistica.combinatoria(0,0))
 
