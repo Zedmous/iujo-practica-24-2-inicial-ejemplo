582e736f6ab102b736930f37e46f16a9c397eddb
Zedmous <zedmous@gmail.com>
2024-11-07 20:19:02 -0400

    x

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ const estadistica = new Matematica();
 console.log("El factorial del numero es: ".green,estadistica.factorial(4));
 console.log("La combinatoria es: ".blue,estadistica.combinatoria(0,0))
 
+console.log("x")
